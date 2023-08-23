d6e45b7765f165bf04c530a4615ba53550ab360d
John Seong <wonmor@gmail.com>
2023-08-23 12:59:52 +0900

    Updated URL

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ef01cfd5939726d"; }
+/******/ 		__webpack_require__.h = function() { return "b17374aadf62d6b8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

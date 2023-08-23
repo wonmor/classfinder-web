e65430b7689dffbcf9138a672903d24b3db119c8
John Seong <wonmor@gmail.com>
2023-08-23 11:24:31 +0900

    UPDATED

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>{`ClassFinder`}</title>
         </Head>
         <Container>
           <Intro />

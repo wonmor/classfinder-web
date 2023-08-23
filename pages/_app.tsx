44828ac7f31c784e30cbc2492abdc66cb30b3078
John Seong <wonmor@gmail.com>
2023-08-23 11:57:30 +0900

    Updated fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import { AppProps } from 'next/app'
-import { Outfit } from 'next/font/google'
-import '../styles/index.css'
+import { AppProps } from "next/app";
+import { Outfit } from "next/font/google";
+import "../styles/index.css";
+
+const outfit = Outfit({ subsets: ["latin"] });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={outfit.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
